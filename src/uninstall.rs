27ee87dd9f125573410ef9351546d326578db375
PinetheApple <pinespace889@gmail.com>
2024-03-20 16:41:38 +0530

    idk what im doing

--- src/uninstall.rs
+++ src/uninstall.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::env;
 
 #[derive(Parser, Debug)]
 #[clap(author = "Pine", version, about)]
@@ -29,13 +30,40 @@ fn _path_exists(_path: &String) -> bool {
 
 fn _delete_config() {}
 
+fn get_search_directories(
+    include_directories: Vec<&str>,
+    exclude_directories: Vec<&str>,
+    base_directories: Vec<&str>,
+) -> Vec<String> {
+    let mut path = "".to_string();
+    match env::var("PATH") {
+        Ok(path_var) => path = path_var,
+        Err(e) => print!(
+            "Encountered an error trying to read the environment variable: {}",
+            e
+        ),
+    };
+    let path_directories: Vec<&str> = path.rsplit(":").collect();
+    let mut search_directories: Vec<String> = Vec::new();
+    if base_directories[0] != "" {
+        for directory in base_directories {
+            search_directories.push(directory.to_owned());
+        }
+        return search_directories;
+    }
+    search_directories
+}
+
 fn main() {
     let args = Arguments::parse();
-    println!("{}", args.program_name);
+
     let exclude_string = args.exclude.unwrap_or("".to_string());
-    let _exclude_directories: Vec<&str> = exclude_string.rsplit(' ').collect();
+    let exclude_directories: Vec<&str> = exclude_string.rsplit(' ').collect();
     let include_string = args.include.unwrap_or("".to_string());
-    let _include_directories: Vec<&str> = include_string.rsplit(' ').collect();
+    let include_directories: Vec<&str> = include_string.rsplit(' ').collect();
     let base_directory_string = args.base_dir.unwrap_or("".to_string());
-    let _base_directories: Vec<&str> = base_directory_string.rsplit(' ').collect();
+    let base_directories: Vec<&str> = base_directory_string.rsplit(' ').collect();
+    let search_directories =
+        get_search_directories(include_directories, exclude_directories, base_directories);
+    println!("{:?}", search_directories);
 }
