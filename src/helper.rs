65be91bdc07481de71d3bdeab0bd3fe631554920
PinetheApple <pinespace889@gmail.com>
2024-03-26 11:12:03 +0530

    moved functions over to helper

--- src/helper.rs
+++ src/helper.rs
@@ -1,4 +1,5 @@
-use std::io::stdin;
+use resolve_path::PathResolveExt;
+use std::{env, io::stdin};
 
 pub fn handle_binary_input() -> bool {
     loop {
@@ -16,4 +17,53 @@ pub fn handle_binary_input() -> bool {
             _ => println!("invalid input, sorry"),
         }
     }
+}
+
+fn path_exists(path: &str) -> bool {
+    match path.try_resolve().unwrap().try_exists() {
+        Ok(true) => {
+            println!("{path}: \t\tExists (0_0)",);
+            return true;
+        }
+        Ok(false) => {
+            println!("{path}: \t\tThis path doesn't exist OR isn't a directory")
+        }
+        Err(_) => {
+            println!("{path}: \t\tOops! some issue locating that directory; Perhaps you don't have sufficient permission? Try running this with root privileges.");
+        }
+    };
+    return false;
+}
+
+pub fn get_search_directories(
+    exclude_directories: Vec<&str>,
+    base_directories: Vec<&str>,
+) -> Vec<String> {
+    let mut search_directories: Vec<String> = Vec::new();
+    if base_directories[0] != "" {
+        for directory in base_directories {
+            if path_exists(directory) {
+                search_directories.push(directory.to_string());
+            }
+        }
+        return search_directories;
+    }
+    let mut path = "".to_string();
+    match env::var("PATH") {
+        Ok(path_var) => path = path_var,
+        Err(e) => print!(
+            "Encountered an error trying to read the $PATH environment variable: {}",
+            e
+        ),
+    };
+    let mut path_directories: Vec<&str> = path.rsplit(":").collect();
+    if exclude_directories[0] != "" {
+        for directory in exclude_directories {
+            path_directories.retain(|&x| x != directory);
+        }
+    }
+    for directory in path_directories {
+        search_directories.push(directory.to_string());
+    }
+    return search_directories;
 }
--- src/uninstall.rs
+++ src/uninstall.rs
@@ -1,10 +1,8 @@
 use clap::Parser;
 use regex::Regex;
-use resolve_path::PathResolveExt;
 use rust_search::{similarity_sort, SearchBuilder};
 use std::process::{exit, Command};
 use std::{
-    env,
     fs::{remove_file, File},
     io::{prelude::*, stdout, Read},
 };
@@ -30,55 +28,6 @@ struct Arguments {
     // /// automatically delete relevant files without asking for confirmation; not a great idea
     // #[arg(short, long, num_args = 0)]
     // auto: bool,
-}
-
-fn path_exists(path: &str) -> bool {
-    match path.try_resolve().unwrap().try_exists() {
-        Ok(true) => {
-            println!("{path}: \t\tExists (0_0)",);
-            return true;
-        }
-        Ok(false) => {
-            println!("{path}: \t\tThis path doesn't exist OR isn't a directory")
-        }
-        Err(_) => {
-            println!("{path}: \t\tOops! some issue locating that directory; Perhaps you don't have sufficient permission? Try running this with root privileges.");
-        }
-    };
-    return false;
-}
-
-fn _get_search_directories(
-    exclude_directories: Vec<&str>,
-    base_directories: Vec<&str>,
-) -> Vec<String> {
-    let mut search_directories: Vec<String> = Vec::new();
-    if base_directories[0] != "" {
-        for directory in base_directories {
-            if path_exists(directory) {
-                search_directories.push(directory.to_string());
-            }
-        }
-        return search_directories;
-    }
-    let mut path = "".to_string();
-    match env::var("PATH") {
-        Ok(path_var) => path = path_var,
-        Err(e) => print!(
-            "Encountered an error trying to read the $PATH environment variable: {}",
-            e
-        ),
-    };
-    let mut path_directories: Vec<&str> = path.rsplit(":").collect();
-    if exclude_directories[0] != "" {
-        for directory in exclude_directories {
-            path_directories.retain(|&x| x != directory);
-        }
-    }
-    for directory in path_directories {
-        search_directories.push(directory.to_string());
-    }
-    return search_directories;
 }
 
 fn handle_config_files(app_name: &str) {}
@@ -128,7 +77,7 @@ fn handle_executable(executable: &str) {
         true => {
             println!("Deleting file...");
             // remove_file(String::from_utf8_lossy(&output))
-            //     .expect("Failed to remove file");
+            //     .expect("Failed to remove file! Are u root?");
             println!("Deleted file successfully \n");
         }
         false => {
